f72b32bf5e30112028b3aab8b8941d1fa20ccea2
freeanser <bnn01615@gmail.com>
2023-07-13 14:25:11 +0800

    feat: add users modules

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const restaurant = require('./modules/restaurant')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/restaurants', restaurant)
+router.use('/users', users)
 
 // 導出此路由
 module.exports = router
