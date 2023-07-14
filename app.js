88dff8dc3d317caee06a807e960e98ddc36f2947
freeanser <bnn01615@gmail.com>
2023-07-14 23:54:13 +0800

    feat: add login feature via passport

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const bodyParser = require('body-parser')
 // 引用自己定義好的設定
 const routes = require('./routes') // = const routes = require('./routes/index')
 const port = 2000
+
+const usePassport = require('./config/passport')
 require('./config/mongoose')
 
 // 使用套件產生的東西
@@ -18,13 +20,16 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(session({
   secret: "ThisIsMySecret",
-  resave: false, // resave: 每次跟使用者互動，都會存新的 session
+  resave: false, // resave: 每次跟使用者互動，都會強制更新 session
   saveUninitialized: true // 儲存新的 session
 }))
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 // use：每一個require都需要來這裡; urlencoded：幫忙解析內容
 app.use(methodOverride('_method'))
+
+usePassport(app) // 因為 passport 最後是輸出一個 function
+
 app.use(routes)
 
 
