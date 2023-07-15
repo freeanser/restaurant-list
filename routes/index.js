04b5116e83d82f0b9b95c7d72a152a3dd8fdd3c7
freeanser <bnn01615@gmail.com>
2023-07-15 10:44:01 +0800

    feat: add user authentication in restaurant and home routers

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,17 @@
 const express = require('express')
 const router = express.Router()
 
+// 引入資源後，記得要使用資源
 const home = require('./modules/home')
 const restaurant = require('./modules/restaurant')
 const users = require('./modules/users')
 
-router.use('/', home)
-router.use('/restaurants', restaurant)
+const { authenticator } = require('../middleware/auth')
+
+// home, todos 都是要驗證後，才能進入的
+router.use('/restaurants', authenticator, restaurant)  // 條件嚴謹的放前面（要符合 /restaurant)
 router.use('/users', users)
+router.use('/', authenticator, home) // 條件寬鬆的放後面（要符合 / 即可）
 
 // 導出此路由
 module.exports = router
